a92f3be4f7a98773c7f7ae3c94b05d725cdae430
Nick Salvatore <nmsalvatore@gmail.com>
2025-01-23 18:30:31 -0800

    feat: implement tests for reformat and validate

---
+++ jellyclean/clean_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from formatting import validate, reformat
+
+
+@pytest.mark.parametrize("original, new", [
+    ("Godzilla.Minus.One.2023.1080p.BluRay.x264.YG", "Godzilla.Minus.One.2023"),
+    ("Wonka (2023) [1080p] [WEBRip] [5.1] [YTS.MX]", "Wonka.2023"),
+    ("2001 A Space Odyssey 1968 Remastered 1080p BluRay", "2001.A.Space.Odyssey.1968"),
+    ("1917.2019.PROPER.1080p.BluRay.x265-RARBG", "1917.2019"),
+    ("Death Race (2000) 1975 720p BluRay Hindi.mkv", "Death.Race.2000.1975.mkv"),
+    ("The.Girl.Next.Door.2004", "The.Girl.Next.Door.2004")
+])
+def test_reformat(original, new):
+    assert reformat(original) == new
+
+
+@pytest.mark.parametrize("title", [
+    "The.Girl.Next.Door.2004",
+    "2001.A.Space.Odyssey.1968",
+    "1917.2019",
+    "Death.Race.2000.1975"
+])
+def test_validate(title):
+    match = validate(title)
+    assert match is not None
+    assert match.group() == title
