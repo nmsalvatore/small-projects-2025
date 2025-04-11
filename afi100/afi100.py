3a75c014b6f33b522f985685683f454b08e585cc
Nick Salvatore <nmsalvatore@gmail.com>
2025-04-11 06:44:39 -0700

    feat: add get_db_connection() function

--- afi100/afi100.py
+++ afi100/afi100.py
@@ -68,37 +68,39 @@ def init_db():
         con.close()
 
 
-def fetch_all_films():
+def get_db_connection():
     con = sqlite3.connect("db.sqlite3")
     con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT * FROM films ORDER BY id")
-    films = cur.fetchall()
-    con.close()
-    return films
+    return con
+
+
+def fetch_all_films():
+    with get_db_connection() as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM films ORDER BY id")
+        films = cur.fetchall()
+        return films
 
 
 def fetch_user_films(list_id):
-    con = sqlite3.connect("db.sqlite3")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("""
-        SELECT
-            f.id,
-            f.title,
-            f.year,
-            f.director,
-            COALESCE(ulf.watched, 0) as watched
-        FROM
-            films f
-        LEFT JOIN
-            user_list_films ulf ON f.id = ulf.film_id AND ulf.list_id = ?
-        ORDER BY
-            f.id
-    """, (list_id,))
-    films = cur.fetchall()
-    con.close()
-    return films
+    with get_db_connection() as con:
+        cur = con.cursor()
+        cur.execute("""
+            SELECT
+                f.id,
+                f.title,
+                f.year,
+                f.director,
+                COALESCE(ulf.watched, 0) as watched
+            FROM
+                films f
+            LEFT JOIN
+                user_list_films ulf ON f.id = ulf.film_id AND ulf.list_id = ?
+            ORDER BY
+                f.id
+        """, (list_id,))
+        films = cur.fetchall()
+        return films
 
 
 @app.route("/")
@@ -115,36 +117,36 @@ def view_private_list(list_id):
 
 @app.route("/toggle-watched/<list_id>/<int:film_id>", methods=["POST"])
 def toggle_watched(list_id, film_id):
-    con = sqlite3.connect("db.sqlite3")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
+    with get_db_connection() as con:
+        cur = con.cursor()
+        cur.execute("SELECT watched FROM user_list_films WHERE list_id = ? AND film_id = ?", (list_id, film_id))
+        user_film_status = cur.fetchone()
 
-    cur.execute("SELECT watched FROM user_list_films WHERE list_id = ? AND film_id = ?", (list_id, film_id))
-    user_film_status = cur.fetchone()
+        if user_film_status is None:
+            cur.execute("INSERT INTO user_list_films VALUES (?, ?, ?)", (list_id, film_id, 1))
 
-    if user_film_status is None:
-        cur.execute("INSERT INTO user_list_films VALUES (?, ?, ?)", (list_id, film_id, 1))
-    else:
-        updated_status = 1 if user_film_status[0] == 0 else 0
-        cur.execute("UPDATE user_list_films SET watched = ? WHERE list_id = ? AND film_id = ?", (updated_status, list_id, film_id))
+        else:
+            updated_status = 1 if user_film_status[0] == 0 else 0
+            cur.execute("UPDATE user_list_films SET watched = ? WHERE list_id = ? AND film_id = ?", (updated_status, list_id, film_id))
 
-    con.commit()
-    cur.execute("""
-        SELECT
-            f.id,
-            f.title,
-            f.year,
-            f.director,
-            COALESCE(ulf.watched, 0) as watched
-        FROM
-            films f
-        LEFT JOIN
-            user_list_films ulf ON f.id = ulf.film_id AND ulf.list_id = ?
-        WHERE
-            f.id = ?
-    """, (list_id, film_id))
-    film = cur.fetchone()
-    con.close()
+        con.commit()
+
+        cur.execute("""
+            SELECT
+                f.id,
+                f.title,
+                f.year,
+                f.director,
+                COALESCE(ulf.watched, 0) as watched
+            FROM
+                films f
+            LEFT JOIN
+                user_list_films ulf ON f.id = ulf.film_id AND ulf.list_id = ?
+            WHERE
+                f.id = ?
+        """, (list_id, film_id))
+
+        film = cur.fetchone()
 
     response = make_response(render_template("film.html", film=film, list_id=list_id))
     response.headers["HX-Trigger"] = "updateProgress"
@@ -160,18 +162,24 @@ def progress(list_id):
 @app.route("/save-list", methods=["POST"])
 def save_list():
     custom_chars = string.ascii_letters + string.digits
-    list_id = nanoid.generate(alphabet=custom_chars, size=8)
+
     watched_film_json = request.form.get("watched_film_ids")
     watched_film_ids = json.loads(watched_film_json)
-    user_list_films = [(list_id, int(film_id), 1) for film_id in watched_film_ids]
 
-    con = sqlite3.connect("db.sqlite3")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("INSERT INTO user_lists (id) VALUES (?)", (list_id,))
-    cur.executemany("INSERT INTO user_list_films (list_id, film_id, watched) VALUES (?, ?, ?)", user_list_films)
-    con.commit()
-    con.close()
+    with get_db_connection() as con:
+        cur = con.cursor()
+
+        while True:
+            list_id = nanoid.generate(alphabet=custom_chars, size=8)
+            cur.execute("SELECT 1 FROM user_lists WHERE id = ?", (list_id,))
+
+            if cur.fetchone() is None:
+                break
+
+        user_list_films = [(list_id, int(film_id), 1) for film_id in watched_film_ids]
+        cur.execute("INSERT INTO user_lists (id) VALUES (?)", (list_id,))
+        cur.executemany("INSERT INTO user_list_films (list_id, film_id, watched) VALUES (?, ?, ?)", user_list_films)
+        con.commit()
 
     return redirect(url_for('view_private_list', list_id=list_id))
 
